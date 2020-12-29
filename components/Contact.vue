7c8e8d73b3b5cbe36a44a17210bc2b714cc5edcd
xyytgae <xyytgae@yahoo.co.jp>
2020-12-29 20:21:54 +0900

    Fix

--- components/Contact.vue
+++ components/Contact.vue
@@ -33,72 +33,64 @@
       method="post"
     >
       <input type="hidden" name="form-name" value="contact" />
-      <!-- <p class="hidden">
-        <label>Don’t fill this out: <input name="bot-field"/></label>
-      </p> -->
-      <!-- <v-form ref="form" v-model="valid"> -->
-      <v-col cols="12" md="3">
-        <v-text-field
-          name="name"
-          outlined
-          shaped
-          :rules="[required]"
-          color="orange"
-          v-model="form.name"
-          label="なまえ"
-        ></v-text-field>
-      </v-col>
-      <v-col cols="12" md="3">
-        <v-text-field
-          name="email"
-          outlined
-          shaped
-          :rules="[required]"
-          color="orange"
-          v-model="form.email"
-          label="Email"
-        ></v-text-field>
-      </v-col>
-      <v-col cols="12" md="3">
-        <v-text-field
-          name="title"
-          outlined
-          shaped
-          :rules="[required]"
-          color="orange"
-          v-model="form.title"
-          label="件名"
-        ></v-text-field>
-      </v-col>
+      <v-form ref="form" v-model="valid">
+        <v-col cols="12" md="3">
+          <v-text-field
+            name="name"
+            outlined
+            shaped
+            :rules="[required]"
+            color="orange"
+            v-model="form.name"
+            label="名前"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" md="3">
+          <v-text-field
+            name="email"
+            outlined
+            shaped
+            :rules="[required]"
+            color="orange"
+            v-model="form.email"
+            label="Email"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" md="3">
+          <v-text-field
+            name="title"
+            outlined
+            shaped
+            :rules="[required]"
+            color="orange"
+            v-model="form.title"
+            label="件名"
+          ></v-text-field>
+        </v-col>
 
-      <v-col cols="12" md="10">
-        <v-textarea
-          name="message"
-          outlined
-          shaped
-          :rules="[required]"
-          color="orange"
-          v-model="form.contents"
-          label="お問い合わせ内容"
-        ></v-textarea>
-      </v-col>
+        <v-col cols="12" md="10">
+          <v-textarea
+            name="message"
+            outlined
+            shaped
+            :rules="[required]"
+            color="orange"
+            v-model="form.contents"
+            label="お問い合わせ内容"
+          ></v-textarea>
+        </v-col>
 
-      <v-col class="text-center">
-        <v-btn
-          type="”submit”"
-          color="orange"
-          :disabled="!valid || loading"
-          :loading="loading"
-          :dark="valid"
-          x-large
-          >送信する</v-btn
-        >
-      </v-col>
-      <!-- 
         <v-col class="text-center">
-          {{ message }}
-        </v-col> -->
-      <!-- </v-form> -->
+          <v-btn
+            type="”submit”"
+            color="orange"
+            :disabled="!valid"
+            :dark="valid"
+            x-large
+            >送信する</v-btn
+          >
+        </v-col>
+      </v-form>
     </form>
   </v-card>
 </template>
@@ -131,31 +123,10 @@ export default {
         contents: '',
       },
       valid: true,
-      loading: false,
+
       required: value => !!value || '必須です',
       message: '送信',
     }
-  },
-  methods: {
-    send() {
-      this.loading = true
-      this.$refs.form.reset()
-      this.message = '送信成功！！'
-    },
-    async submit() {
-      const params = new FormData()
-      //以下、ダミーフォームの各フォーム要素のnameと合わせる
-      params.append('form-name', 'contact')
-      params.append('name', this.name)
-      params.append('email', this.email)
-      params.append('company', this.company)
-      params.append('message', this.message)
-      params.append('bot-field', this.botfield)
-
-      const response = await this.$axios.$post(window.location.origin, params)
-      //実際はresponseを使って画面側にフィードバックさせるが、ここでは仮にconsoleに出力
-      console.log(response)
-    },
   },
 }
 </script>
