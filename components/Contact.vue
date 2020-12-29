29cbf97c1f589d7e8d490540cdbce079526262df
xyytgae <xyytgae@yahoo.co.jp>
2020-12-29 18:14:35 +0900

    Fix netlify対応フォームを追加

--- components/Contact.vue
+++ components/Contact.vue
@@ -4,6 +4,32 @@
     <v-card-text class="py-0 text-center headline font-weight-bold"
       ><span class="menu">お問い合わせ</span></v-card-text
     >
+
+    <form name="contact" method="POST" data-netlify="true">
+      <input type="hidden" name="form-name" value="contact" />
+      <p>
+        <label>Your Name: <input type="text" name="name"/></label>
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email"/></label>
+      </p>
+      <p>
+        <label
+          >Your Role:
+          <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select></label
+        >
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+
     <form name="contact" method="POST" netlify>
       <v-form ref="form" v-model="valid">
         <!-- <v-container> -->
