327a47c56985548cd6e5990dca9e776979668694
xyytgae <xyytgae@yahoo.co.jp>
2020-12-29 17:59:35 +0900

    Add

--- components/Contact.vue
+++ components/Contact.vue
@@ -4,75 +4,77 @@
     <v-card-text class="py-0 text-center headline font-weight-bold"
       ><span class="menu">お問い合わせ</span></v-card-text
     >
-    <v-form ref="form" v-model="valid">
-      <!-- <v-container> -->
+    <form name="contact" method="POST" netlify>
+      <v-form ref="form" v-model="valid">
+        <!-- <v-container> -->
 
-      <v-col cols="12" md="3">
-        <v-text-field
-          outlined
-          shaped
-          :rules="[required]"
-          color="orange"
-          v-model="form.name"
-          label="なまえ"
-        ></v-text-field>
-      </v-col>
-      <v-col cols="12" md="3">
-        <v-text-field
-          outlined
-          shaped
-          :rules="[required]"
-          color="orange"
-          v-model="form.email"
-          label="Email"
-        ></v-text-field>
-      </v-col>
-      <v-col cols="12" md="3">
-        <v-text-field
-          outlined
-          shaped
-          :rules="[required]"
-          color="orange"
-          v-model="form.title"
-          label="件名"
-        ></v-text-field>
-      </v-col>
+        <v-col cols="12" md="3">
+          <v-text-field
+            outlined
+            shaped
+            :rules="[required]"
+            color="orange"
+            v-model="form.name"
+            label="なまえ"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" md="3">
+          <v-text-field
+            outlined
+            shaped
+            :rules="[required]"
+            color="orange"
+            v-model="form.email"
+            label="Email"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" md="3">
+          <v-text-field
+            outlined
+            shaped
+            :rules="[required]"
+            color="orange"
+            v-model="form.title"
+            label="件名"
+          ></v-text-field>
+        </v-col>
 
-      <v-col cols="12" md="10">
-        <v-textarea
-          outlined
-          shaped
-          :rules="[required]"
-          color="orange"
-          v-model="form.contents"
-          label="お問い合わせ内容"
-        ></v-textarea>
-      </v-col>
+        <v-col cols="12" md="10">
+          <v-textarea
+            outlined
+            shaped
+            :rules="[required]"
+            color="orange"
+            v-model="form.contents"
+            label="お問い合わせ内容"
+          ></v-textarea>
+        </v-col>
 
-      <v-col class="text-center">
-        <v-btn
-          color="orange"
-          :disabled="!valid || loading"
-          :loading="loading"
-          :dark="valid"
-          x-large
-          @click="send"
-          >送信する</v-btn
-        >
-      </v-col>
+        <v-col class="text-center">
+          <v-btn
+            color="orange"
+            :disabled="!valid || loading"
+            :loading="loading"
+            :dark="valid"
+            x-large
+            @click="send"
+            >送信する</v-btn
+          >
+        </v-col>
 
-      <v-col class="text-center">
-        {{ message }}
-      </v-col>
+        <v-col class="text-center">
+          {{ message }}
+        </v-col>
 
-      <!-- </v-container> -->
-    </v-form>
+        <!-- </v-container> -->
+      </v-form>
+    </form>
   </v-card>
 </template>
 
 <script>
 // import firebase from '~/plugins/firebase.js'
-import { functions } from '@/plugins/firebase'
+// import { functions } from '@/plugins/firebase'
 
 export default {
   data() {
